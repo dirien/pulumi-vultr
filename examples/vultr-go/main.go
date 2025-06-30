029948c0e16a2e8b07590247b4f7c9fecd1472cd
Engin Diri <engin.diri@ediri.de>
2025-06-30 12:56:02 +0200

    Upgrade terraform provider vultr to v2.26.0 (#552)
    * make tfgen
    
    * make tfgen
    
    * make generate_sdks
    
    * feat: add stuff
    
    * feat: add stuff

--- examples/vultr-go/main.go
+++ examples/vultr-go/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		vke, err := vultr.NewKubernetes(ctx, "vke", &vultr.KubernetesArgs{
+		/*vke, err := vultr.NewKubernetes(ctx, "vke", &vultr.KubernetesArgs{
 			Region:  pulumi.String("fra"),
 			Version: pulumi.String("v1.25.4+1"),
 			Label:   pulumi.String("pulumi-vultr"),
@@ -22,6 +22,19 @@ func main() {
 			return err
 		}
 		ctx.Export("kubeconfig", vke.KubeConfig)
+		*/
+
+		vm, err := vultr.NewInstance(ctx, "vultr", &vultr.InstanceArgs{
+			Label:      pulumi.String("pulumi-vultr"),
+			Plan:       pulumi.String("vc2-1c-1gb"),
+			Region:     pulumi.String("fra"),
+			OsId:       pulumi.Int(1743),
+			EnableIpv6: pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+		ctx.Export("instanceId", vm.ID())
 		return nil
 	})
 }
